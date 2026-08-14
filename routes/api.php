f699a99b96c99e6277f1c06eacb5fcc9ca71975d
Jose Dalanon <josedalanon630@gmail.com>
2026-08-14 16:06:36 +0800

    removed test endpoint on api.php

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,3 @@ Route::middleware('auth:sanctum')->group(function () {
     // table name is the endpoints of each sources
     Route::apiResource(with(new Post)->getTable(), PostController::class);
 });
-
-/*
-    ! test only very dangerous
-*/
-Route::get('/tables', function () {
-    $models = collect(Finder::create()->files()->in(app_path('Models')))
-        ->map(function ($file) {
-            return
-                'App\\Models\\' . Str::replace(
-                    ['/', '.php'],
-                    ['\\', ''],
-                    Str::after($file->getRealPath(), app_path('Models') . DIRECTORY_SEPARATOR)
-                );
-        })
-        ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Database\Eloquent\Model::class) && (new $class)->getTable() !== 'users')
-        ->mapWithKeys(fn($class) => [Str::headline(class_basename($class)) => (new $class)->getTable()]);
-
-    return $models->keys();
-});
